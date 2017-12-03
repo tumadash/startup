58242dff91dbdbd02ae54f4e9721376551f6e2d4
tumadash <dasha.tumanova@inbox.ru>
2017-12-03 18:06:33 +0300

    Startup-frontend 3. Роутинг.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,15 @@ import {RouterModule} from '@angular/router';
 import {LoginComponent} from './pages/login/login.component';
 import {VoyageConditionComponent} from './components/voyage-condition/voyage-condition.component';
 import {AddVoyageComponent} from './components/add-voyage/add-voyage.component';
+import {AddMotorShipComponent} from './components/add-motor-ship/add-motor-ship.component';
+import {AddPortComponent} from './components/add-port/add-port.component';
 
 const routes = [
     {path: 'general', component: GeneralComponent},
     {path: 'general/voyage-condition', component: VoyageConditionComponent},
     {path: 'general/add-voyage', component: AddVoyageComponent},
+    {path: 'general/add-motor-ship', component: AddMotorShipComponent},
+    {path: 'general/add-port', component: AddPortComponent},
     {path: 'login', component: LoginComponent},
     {path: '', redirectTo: '/login', pathMatch: 'full'}];
 
